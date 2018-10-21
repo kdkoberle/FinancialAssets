ff770c65b91a14a547c5a172c9b932c748c801c8
Keith K <kdkoberle@madisoncollege.edu>
2018-10-21 16:48:47 -0500

    basics for daofactorie done

--- src/main/java/com/financialassets/persistence/DaoFactory.java
+++ src/main/java/com/financialassets/persistence/DaoFactory.java
@@ -16,7 +16,6 @@ public class DaoFactory<T> {
     private Class<T> type;
 
     private final Logger logger = LogManager.getLogger(this.getClass());
-    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     /**
      * Instantiates a new Dao factory.
@@ -27,8 +26,86 @@ public class DaoFactory<T> {
         this.type = type;
     }
 
+
+    /**
+     * create and return an opened session from SessionFactory
+     *
+     */
     private Session getSession() {
         return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
+
+    /**
+     * Gets entity type by id.
+     *
+     * @param <T> the type parameter
+     * @param id  the id
+     * @return the databaseEntity of specified type by id
+     */
+    public <T>T getById(int id) {
+        Session session = getSession();
+        T databaseEntity = (T)session.get(type, id);
+        session.close();
+        return databaseEntity;
+    }
+
+    /**
+     * Delete.
+     *
+     * @param entity the entity to be deleted
+     */
+    public void delete(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(entity);
+        transaction.commit();
+        session.close();
+    }
+
+
+    /**
+     * Gets all.
+     *
+     * @return List of all entities of type T
+     */
+    public List<T> getAll() {
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<T> allT = session.createQuery(query).getResultList();
+        session.close();
+        return allT;
+    }
+
+    /**
+     * Save or update.
+     *
+     * @param  entity of type T to be updated
+     */
+    public void saveOrUpdate(T entity) {
+        Session session = getSession();
+        session.saveOrUpdate(entity);
+        session.close();
+    }
+
+
+    /**
+     * Insert int.
+     *
+     * @param entity the entity
+     * @return the assigned id of new entry
+     */
+    public int insert(T entity) {
+        int insertedId = 0;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        insertedId = (int)session.save(entity);
+        transaction.commit();
+        session.close();
+
+        return insertedId;
     }
 
 }
