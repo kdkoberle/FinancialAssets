20a5ce55528db227f6eaa4fc449e2cb187ca3f85
Keith K <kdkoberle@gmail.com>
2018-12-02 20:04:10 -0600

    working on api usage

---
+++ src/main/java/com/financialassets/apiclient/IEXClient.java
@@ -0,0 +1,11 @@
+package com.financialassets.apiclient;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IEXClient {
+}
