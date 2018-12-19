4488a09c8fa2f8fa900ca5fbeed0ae0549ddaa2d
Keith K <kdkoberle@gmail.com>
2018-12-19 14:19:46 -0600

    cleandb

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,3 +1,8 @@
+SET foreign_key_checks = 0;
+DELETE FROM Role;
+DELETE FROM User;
+DELETE FROM UserAsset;
+SET foreign_key_checks = 1;
 
 create table user
 (
@@ -15,27 +20,6 @@ create table user
 alter table user
   add primary key (user_id);
 
-create table user_asset
-(
-  user_asset_id int auto_increment,
-  buy_price     double(13, 2) not null,
-  sell_price    double(13, 2) null,
-  buy_date      date          not null,
-  sell_date     date          null,
-  asset_name    varchar(20)   not null,
-  user_id       int           not null,
-  asset_id      int           not null,
-  qty           int           null,
-  constraint user_asset_user_asset_id_uindex
-    unique (user_asset_id),
-  constraint user_asset_user_id_fk
-    foreign key (user_id) references user (user_id)
-      on delete cascade
-);
-
-alter table user_asset
-  add primary key (user_asset_id);
-
 create table role
 (
   id      int auto_increment,
@@ -49,19 +33,37 @@ create table role
   constraint role_user_id_uindex
     unique (user_id),
   constraint role_user_id_fk
-    foreign key (id) references user (user_id)
+    foreign key (user_id) references user (user_id)
       on delete cascade
 );
 
 alter table role
   add primary key (id);
 
+create table user_asset
+(
+  user_asset_id int auto_increment,
+  buy_price     double(13, 2) not null,
+  sell_price    double(13, 2) null,
+  buy_date      date          not null,
+  sell_date     date          null,
+  asset_name    varchar(20)   not null,
+  user_id       int           not null,
+  qty           int           not null,
+  fees          double(13, 2) not null,
+  constraint user_asset_user_asset_id_uindex
+    unique (user_asset_id),
+  constraint user_asset_user_id_fk
+    foreign key (user_id) references user (user_id)
+      on delete cascade
+);
+
+alter table user_asset
+  add primary key (user_asset_id);
 
 
-INSERT INTO user (user_id, email, first_name, last_name, password) VALUES (1, 'kdkoberle@madisoncollege.edu', 'Keith', 'Koberle', '123456');
-INSERT INTO user (user_id, email, first_name, last_name, password) VALUES (2, 'jimmy@johns.com', 'Jimmy', 'John', '456789');
-INSERT INTO role(email, user_id, role) VALUES ('kdkoberle@madisoncollege.edu', 1, 'admin');
-INSERT INTO user_asset (user_asset_id, buy_price, sell_price, buy_date, sell_date, asset_name, user_id, asset_id) VALUES (4, 13.51, 54.21, '2015-11-12', '2018-11-11', 'INTC', 1, 1);
-INSERT INTO user_asset (user_asset_id, buy_price, sell_price, buy_date, sell_date, asset_name, user_id, asset_id) VALUES (5, 14.58, null, '2018-11-08', null, 'Silver', 1, 2);
-INSERT INTO user_asset (user_asset_id, buy_price, sell_price, buy_date, sell_date, asset_name, user_id, asset_id) VALUES (6, 15.21, null, '2018-11-01', null, 'Silver', 1, 2);
-INSERT INTO user_asset (user_asset_id, buy_price, sell_price, buy_date, sell_date, asset_name, user_id, asset_id) VALUES (7, 1225.21, null, '2018-10-10', null, 'Gold', 1, 3);
+INSERT INTO `user` VALUES (1,'kdkoberle@madisoncollege.edu','Keith','Koberle','123456');
+INSERT INTO `user_asset` VALUES (4,13.51,54.21,'2015-11-12','2018-11-11','INTC',1,15,9.90),(5,14.58,NULL,'2018-11-08',NULL,'TSLA',1,100,4.95),(6,15.21,NULL,'2018-11-01',NULL,'STM',1,115,4.95),(7,5.15,11.73,'2018-10-10','2018-12-19','AU',1,75,9.90),(28,14.50,NULL,'2018-12-19',NULL,'AG',1,50,4.95);
+INSERT INTO `role` VALUES (1,'kdkoberle@madisoncollege.edu',1,'admin');
+
+
